cc07dcb09d15698abf48b5572c4edbc2a2de1194
Smit Shah <smits@zillowgroup.com>
2020-08-20 12:34:40 -0700

    removing basic_test.py resolved #25

--- luminaire/tests/basic_test.py
+++
@@ -1,8 +0,0 @@
-# write functional, low overhead tests like this
-def test_has_tests():
-    pass
-    # assert_true (False, "There aren't any tests.")
-
-
-def test_import_gitlab_egg_test():
-    import luminaire  # noqa: F401 - suppress one type of error on this line
