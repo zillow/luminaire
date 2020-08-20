bb7e2b57decc69518f01a472b95396463fa7cbbe
Smit Shah <smits@zillowgroup.com>
2020-08-20 12:05:12 -0700

    updating version

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='luminaire',
-    version='0.1.0.dev8',
+    version='0.1.0.dev9',
 
     license='Apache License 2.0',
 
