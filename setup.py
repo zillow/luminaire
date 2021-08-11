9df8c3ae76b3b9644238a275cc8dd180c3951e1a
sayanc <sayanc@zillowgroup.com>
2021-08-10 18:10:10 -0700

    version updated for the next release

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='luminaire',
-    version='0.2.2',
+    version='0.2.3',
 
     license='Apache License 2.0',
 
