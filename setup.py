2ff2446898c71d5484952e80c67ff901f3b70353
sayanc <sayanc@zillowgroup.com>
2022-02-07 18:14:56 -0800

    python version upgrade for ci/cd

--- setup.py
+++ setup.py
@@ -25,15 +25,17 @@ setup(
     author='Zillow Group A.I. team',
     author_email='luminaire-dev-oss@zillowgroup.com',
 
-    python_requires='>=3.6',
+    python_requires='>=3.7',
     packages=find_packages(),
     install_requires=install_requires,
 
     classifiers=[
         'Development Status :: 4 - Beta',
         'License :: OSI Approved :: Apache Software License',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
         'Operating System :: OS Independent',
         'Topic :: Scientific/Engineering :: Information Analysis',
         'Topic :: Software Development :: Libraries :: Python Modules',
