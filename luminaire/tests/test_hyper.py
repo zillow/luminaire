9a567dd1654054520edebdc0802488a93ef722e4
Panagiotis Papaemmanouil <papaemman.pan@gmail.com>
2023-01-23 15:24:39 +0200

    add rstate for hyperparameter optimization and add a test for it

--- luminaire/tests/test_hyper.py
+++ luminaire/tests/test_hyper.py
@@ -2,9 +2,16 @@ from luminaire.optimization.hyperparameter_optimization import *
 
 class TestHyperparameterOptimization(object):
 
-    def test_run(self, test_data_with_missing):
-
+    def test_run1(self, test_data_with_missing):
+        """Test using the default random_state=None"""
         hyper_obj = HyperparameterOptimization(freq='D', detection_type='OutlierDetection')
+        hyper_parameters = hyper_obj.run(test_data_with_missing, max_evals=5)
+
+        assert isinstance(hyper_parameters, dict)
+
+    def test_run2(self, test_data_with_missing):
+        """Test defining a random_state"""
+        hyper_obj = HyperparameterOptimization(freq='D', detection_type='OutlierDetection', random_state=42)
         hyper_parameters = hyper_obj.run(test_data_with_missing, max_evals=5)
 
         assert isinstance(hyper_parameters, dict)
